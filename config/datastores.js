8f6abe6f22b7e9dec76b145ac6d54c3de95e3ede
xKrystalx <xKrystalx@users.noreply.github.com>
2021-05-08 22:55:30 +0200

    Update datastores.js

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: require('sails-mongo'),
-    url: 'mongodb://root@192.168.2.100/local',
+    url: 'mongodb://root@192.168.2.2/local',
   },
 
 
